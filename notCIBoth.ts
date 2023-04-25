faab1f40520ab7e37be6f80a07c188d4c9f63b94
ajaysumfactor <ajayverma.sumfactor@gmail.com>
2023-04-25 16:47:07 +0530

    q9 and not common in both

---
+++ notCIBoth.ts
@@ -0,0 +1,36 @@
+function commonBoth(firstArray : number[],secondArray : number[]): number[]
+{
+   
+     const maxLengthArray: number[] = (firstArray.length > secondArray.length) ? firstArray : secondArray;
+     const minLengthArray: number[]=  (firstArray.length > secondArray.length) ? secondArray: firstArray;
+
+
+     let arr : number[]=[];
+     let myMap=new Map<number,number>();
+     for(let idx=0;idx<maxLengthArray.length;idx++){
+        myMap.set(maxLengthArray[idx],0);
+     }
+
+     for(let idx=0;idx<minLengthArray.length;idx++){
+        let n=minLengthArray[idx];
+        if(myMap.has(n)){
+            arr.push(n);
+            myMap.delete(n);
+        }
+     }
+
+     
+    let concatedArray: number[]=Array.from(new Set(firstArray.concat(secondArray)));
+    console.log(concatedArray);
+    let notCommon:number[]=[];
+    for(let i=0;i<concatedArray.length;i++){
+        if(!arr.includes(concatedArray[i])){
+            notCommon.push(concatedArray[i]);
+        }
+    }
+    
+    return notCommon;
+     
+}
+console.log(commonBoth([5,5,9,8,5,5,8,0,3],[9,7,1,0,3,6,5,9,1,1,8,0,2,4,2,9,1,5]));
+
--- q9.ts
+++ q9.ts
@@ -1,16 +1,31 @@
 //9. Write a function that takes a string as input and returns true if the string is a palindrome, and false otherwise.
-function checkPalindrome(str : string): boolean{
-    let start=0;
-    let end=str.length-1;
+//first method
 
-    while(start<end){
-        if(str.charAt(start)!=str.charAt(end)){
-            return false;
-        }
-        start++;
-        end--;
-    }
-    return true;
+// function checkPalindrome(str : string): boolean{
+//     let start=0;
+//     let end=str.length-1;
+
+//     while(start<end){
+//         if(str.charAt(start)!=str.charAt(end)){
+//             return false;
+//         }
+//         start++;
+//         end--;
+//     }
+//     return true;
+// }
+
+// console.log(checkPalindrome("asdfgfdsa"));
+
+
+
+//second method 
+
+
+function ispalindrome(str : string) : boolean{
+    let reverse=str.split('').reverse().join('');
+    return reverse===str;
 }
 
-console.log(checkPalindrome("asdfgfdsa"));
+console.log(ispalindrome("asdfghgfdsa"));
+export{}
